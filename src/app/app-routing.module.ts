d5bb4cbe4d9c51f89c61c6d5490e699af7a5bcc9
samuelsundarn <samuelsundarn@gmail.com>
2025-01-12 15:12:06 +0530

    Hello

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+},
+{
+    path:'home',
+    component:HomeComponent
+},
+{
+    path:'login',
+    component:LoginComponent
+},
+{
+    path:'register',
+    component:RegisterComponent
+},
+{
+    path:'jobs',
+    component:JobsComponent
+},
+{
+    path:'**',
+    component:HomeComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration, withEventReplay } from '@angular
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { JobsComponent } from './pages/jobs/jobs.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    JobsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
